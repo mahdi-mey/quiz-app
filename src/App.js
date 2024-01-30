b8aca24bc6bf15b7fcf4d65d85effa31311459f7
Mahdi <mahdimey17@gmail.com>
2024-01-30 23:47:10 +0330

    added api and fetched data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {useEffect} from 'react'
 import './App.css';
-import DateCounter from './DateCounter';
+import Header from './Header'
+import Main from './Main'
 
 function App() {
+  useEffect(function(){
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())
+        .then(data => console.log(data))
+          .catch(err => console.error(err))
+  })
   return (
     <div className="App">
-      <DateCounter />
+      <Header></Header>
+      <Main>
+        <p>1/10</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
