f92f4dae84d30688547ff4d27f1f1d2b548d3c16
Mahdi <mahdimey17@gmail.com>
2024-01-31 17:58:30 +0330

    added reducer function with 2 cases & initialState&

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import {useEffect} from 'react'
+import {useEffect, useReducer} from 'react'
 import './App.css';
 import Header from './Header'
 import Main from './Main'
 
+const initialState = {
+  questions: [],
+  status: 'loading' // loading, error, ready, active, finished
+}
+
+function reducer(prevState, action){
+  switch(action.type){
+    case 'dataReceived':
+      return {
+        ...prevState,
+        questions: action.payload,
+        status: 'ready'
+      }
+    case 'dataFailed':
+      return {
+        ...prevState,
+        status: 'error'
+      }
+    default: throw new Error('Action unknown')
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   useEffect(function(){
     fetch('http://localhost:8000/questions')
       .then(res => res.json())
-        .then(data => console.log(data))
-          .catch(err => console.error(err))
+        .then(data => dispatch({type: 'dataReceived', payload: data}))
+          .catch(err => dispatch({type: 'dataFailed'}))
   })
   return (
     <div className="App">
