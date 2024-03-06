dd6e7d8d29f46a2c26b5b35a94e0b8180a6e84d7
Mahdi <mahdimey17@gmail.com>
2024-03-06 22:19:49 +0330

    fixed importing issue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import QuizContext from './contextes/QuizContext'
+import { QuizProvider } from './contextes/QuizContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <QuizContext>
+    <QuizProvider>
       <App />
-    </QuizContext>
+    </QuizProvider>
   </React.StrictMode>
 )
